4e97d6ba8f282b12f719e7e042df9cf015b1882d
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-16 20:39:04 +0330

    Some setup for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import {
+  Launcher,
+  AddPlayers,
+  // RandomizeRoles,
+  // SelectingRoles,
+} from "./components/pages/exporter";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
 export default function App() {
+  const players = useSelector((core) => core.players);
+
+  useEffect(() => {
+    localStorage.setItem("players", JSON.stringify(players));
+  }, [players]);
+
   return (
     <>
-      App
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<Launcher />} />
+          <Route path='add-players' element={<AddPlayers />} />
+          {/* <Route path='selecting-roles' element={<SelectingRoles />} /> */}
+          {/* <Route path='randomize-roles' element={<RandomizeRoles />} /> */}
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
